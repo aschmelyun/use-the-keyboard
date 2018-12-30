1d695ec12e66a44ffc040584c6d28117474701d2
Andrew Schmelyun <schmelyun@gmail.com>
2018-12-29 20:33:08 -0500

    GA added

--- cache/b6ad8b3c72773c30f9e66e4d6fd8e296b9e2fbe6.php
+++ cache/b6ad8b3c72773c30f9e66e4d6fd8e296b9e2fbe6.php
@@ -53,4 +53,14 @@
         }
     });
 </script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-131513064-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-131513064-1');
+</script>
+
 <?php echo $__env->yieldContent('bottom-scripts'); ?>
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -52,4 +52,14 @@
         }
     });
 </script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-131513064-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-131513064-1');
+</script>
+
 @yield('bottom-scripts')
