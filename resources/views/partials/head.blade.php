295337c888e3307aa29b0592e317c20079a252f1
Andrew Schmelyun <schmelyun@gmail.com>
2019-01-03 17:15:48 -0500

    Renames social share image

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -14,13 +14,13 @@
     <meta property="og:description" content="{{ $meta_description ?? '' }}" />
     <meta property="og:url" content="https://usethekeyboard.com/{{ $slug === '/' ? '' : $slug }}" />
     <meta property="og:site_name" content="UseTheKeyboard" />
-    <meta property="og:image" content="https://usethekeyboard.com/assets/img/undraw-taking-notes.png" />
+    <meta property="og:image" content="https://usethekeyboard.com/assets/img/undraw-taking-notes-square.png" />
 
     <meta name="twitter:card" content="summary" />
     <meta name="twitter:description" content="{{ $meta_description ?? '' }}" />
     <meta name="twitter:title" content="{{ $meta_title ?? 'Keyboard shortcuts for Mac apps, Windows programs, and websites' }}" />
     <meta name="twitter:site" content="@aschmelyun" />
-    <meta name="twitter:image" content="https://usethekeyboard.com/assets/img/undraw-taking-notes.png" />
+    <meta name="twitter:image" content="https://usethekeyboard.com/assets/img/undraw-taking-notes-square.png" />
 
     <link rel="apple-touch-icon" sizes="180x180" href="/assets/img/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon-32x32.png">
