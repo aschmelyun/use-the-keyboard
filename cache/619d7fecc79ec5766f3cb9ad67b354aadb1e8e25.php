86d5a6193d893bc273a615e59519ca20325c070b
Andrew Schmelyun <schmelyun@gmail.com>
2019-09-05 23:04:46 -0400

    Updates styles for better color contrast

--- cache/619d7fecc79ec5766f3cb9ad67b354aadb1e8e25.php
+++
@@ -1,8 +0,0 @@
-<?php $__env->startSection('content'); ?>
-    <div class="row">
-        <div class="col-sm-12 col-md-4 offset-md-4">
-            <h1>Taro</h1>
-        </div>
-    </div>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layout.default', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
