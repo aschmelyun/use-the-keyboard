90fb68adf9b6b849c7dee9f03b7da558738a6c2d
Andrew Schmelyun <schmelyun@gmail.com>
2020-06-12 14:31:23 -0400

    Removes Google AdSense and Analytics

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -56,13 +56,4 @@
     });
 </script>
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-131513064-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'UA-131513064-1');
-</script>
-
 @yield('bottom-scripts')
